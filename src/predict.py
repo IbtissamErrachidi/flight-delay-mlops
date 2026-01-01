0991241e010d714f2d20b71487b8f9bb81664ebf
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2026-01-01 01:07:11 +0100

    feat(api): update prediction logic

--- src/predict.py
+++ src/predict.py
@@ -1,39 +1,29 @@
 import pandas as pd
 import joblib
 from src.features.datetime_features import add_datetime_features_predire
-from src.features.historical_features import add_historical_features
+from src.models.utils import add_fold_features_arr
 
-
-# Charger le modèle et l'encodeur
+# Charger les ressources
+feature_names = joblib.load("models/feature_names.pkl")
 model = joblib.load("models/best_model.pkl")
 encoder = joblib.load("models/encoder.pkl")
 
-# Charger X_train et y_train pour les features historiques
+
 X_train = pd.read_csv("data/processed/X_train.csv")
-y_train = pd.read_csv("data/processed/y_train.csv")
-
-
+y_train = pd.read_csv("data/processed/y_train.csv").squeeze() 
 
 cat_cols = ['airline', 'origin_airport', 'dest_airport']
 
 def predict(flight_input):
-    # Convertir l'entrée en DataFrame
     X_new = pd.DataFrame([flight_input.dict()])
-
-    # Ajouter features datetime
     X_new = add_datetime_features_predire(X_new)
-
-    # Ajouter features historiques
-    X_new = add_historical_features(X_train, y_train, X_new)
-
-    # Encoder les colonnes catégorielles
+    
     X_new[cat_cols] = X_new[cat_cols].astype(str)
     X_new[cat_cols] = encoder.transform(X_new[cat_cols])
-
-    # Faire la prédiction
+    
+    _, X_new = add_fold_features_arr(X_train, y_train.to_frame(), X_new)
+    
+    X_new = X_new[feature_names]
     pred_delay = model.predict(X_new)[0]
-
+    
     return pred_delay
-
-
-
